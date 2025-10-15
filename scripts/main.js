e726b17924e6933fb297624a676a36d9d0bcbd81
GraziellaCarvalho <grazicarvalho3105@gmail.com>
2025-10-15 16:17:05 -0300

    Entrega II - Adição de CSS responsivo e design system

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+const menuBtn = document.getElementById("menu-btn");
+const menuList = document.getElementById("menu-list");
+
+menuBtn.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+});
+
